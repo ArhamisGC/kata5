ee31ae638fca04cc80378e9a4227b49bfe91fef2
hunte <hunterofseries13@gmail.com>
2024-01-20 02:17:51 +0000

    first commit

---
+++ src/main/java/kata5/Command.java
@@ -0,0 +1,12 @@
+package kata5;
+
+public interface Command {
+    Output execute(Input input);
+    interface Input{
+        String get(String key);
+    }
+    interface Output{
+        String result ();
+        int response();
+    }
+}
